a54006d850b9de1892897976718adb3cc4eed526
Math Practice Server <mathpractice@example.com>
2026-02-25 14:11:30 +0800

    add monkey-jj.py

---
+++ monkey-jj.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import requests
+
+r = requests.get("http://qt.gtimg.cn/q=sh000001,sz159611,sh562500")
+
+
+# r = requests.get("http://hq.sinajs.cn/list=sz002581,sh601168,sz000158,sh603919,sz002665")
+
+response = r.text
+
+strList = response.split("\n")
+
+profit = 0
+
+for one in strList:
+    if one == '':
+        break
+    options = one.split('=')[1][1:].split('~')
+    name = options[1]
+    start = options[4]
+    now = options[3]
+
+    startFloat = float(start)
+    nowFloat = float(now)
+    range = (nowFloat - startFloat) / startFloat
+    range = str(round(range * 100, 2))
+    print(name + "\topen:" + start + "\tnow:" + now + "\trange:" + range + "%")
+
+profit = str(round(profit, 2))
