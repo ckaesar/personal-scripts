36df82b5aac7adbeaa18748fb6b0fc326822cdd0
Math Practice Server <mathpractice@example.com>
2026-04-10 14:06:35 +0800

    add runzekeji yingweike

--- monkey.py
+++ monkey.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import requests
 
-r = requests.get("http://qt.gtimg.cn/q=sh000001,sz300039,sz001289,sz300812,sz000977")
+r = requests.get("http://qt.gtimg.cn/q=sh000001,sz300039,sz001289,sz300812,sz000977,sz300442,sz002837")
 
 
 # r = requests.get("http://hq.sinajs.cn/list=sz002581,sh601168,sz000158,sh603919,sz002665")
