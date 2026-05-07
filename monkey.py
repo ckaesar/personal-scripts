ca10fe65eae475203ca54bf13e4e411728ca0d3f
Math Practice Server <mathpractice@example.com>
2026-05-07 20:43:19 +0800

    update

--- monkey.py
+++ monkey.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import requests
 
-r = requests.get("http://qt.gtimg.cn/q=sh000001,sz002837,sh600110,sz300346,sz002185,sz000762,sh688516,sz000021,sz300890")
+r = requests.get("http://qt.gtimg.cn/q=sh000001,sz002837,sz300346,sz002185,sz000762,sz000021,sz002460,sh600203,sz002456,sz300342,sh605488,sz000880")
 
 
 # r = requests.get("http://hq.sinajs.cn/list=sz002581,sh601168,sz000158,sh603919,sz002665")
