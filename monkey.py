fc1afcef56932e347f83edf8e268050c77735f75
ckaesar <a502165083@126.com>
2026-05-20 15:08:00 +0800

    add hailanxin; del baobiandianqi

--- monkey.py
+++ monkey.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import requests
 
-r = requests.get("http://qt.gtimg.cn/q=sh000001,sz002837,sz300346,sz002460,sz002412,sz300809,sz000021,sz002185,sh603212,sh600875,sz000066")
+r = requests.get("http://qt.gtimg.cn/q=sh000001,sz002837,sz300346,sz002460,sz002412,sz002185,sh600875,sz000066,sz002048,sz300065")
 
 
 # r = requests.get("http://hq.sinajs.cn/list=sz002581,sh601168,sz000158,sh603919,sz002665")
