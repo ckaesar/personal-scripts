2765906fb72fa19cab8318a5517e047106bbd7c1
ckaesar <a502165083@126.com>
2026-08-27 17:05:25 +0800

    update

--- monkey-bk.py
+++ monkey-bk.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import requests
 
-r = requests.get("http://qt.gtimg.cn/q=sh000001,sz002675,sz002185,sz002637,sh601898,sh600096,sh600522,sh600884,sh600110")
+r = requests.get("http://qt.gtimg.cn/q=sh000001,sh603993,sz002049")
 
 
 # r = requests.get("http://hq.sinajs.cn/list=sz002581,sh601168,sz000158,sh603919,sz002665")
